e6f4e591f152e3771096bad6260f2cc28479c505
Kaio Magalhães <kaio.zeroglosa@gmail.com>
2015-05-06 19:16:30 -0300

    finalizado

---
+++ persistencia/30_abril/2/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Created by zeroglosa on 06/05/15.
+ */
+public class Main {
+
+	public static void main(String[] args){
+
+		for(int i = 0;i<4;i++){
+			new VetorThread().start();
+		}
+
+
+	}
+
+
+
+
+
+
+
+}
